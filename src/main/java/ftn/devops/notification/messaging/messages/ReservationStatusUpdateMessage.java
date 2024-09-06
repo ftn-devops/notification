067febffa1bf816a8e08c05457ec79bb7341b420
Nadja Gvozdenac <gvozdenacn@gmail.com>
2024-09-06 16:25:40 +0200

    Add annotations on messages.

--- src/main/java/ftn/devops/notification/messaging/messages/ReservationStatusUpdateMessage.java
+++ src/main/java/ftn/devops/notification/messaging/messages/ReservationStatusUpdateMessage.java
@@ -1,5 +1,14 @@
 package ftn.devops.notification.messaging.messages;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
 public class ReservationStatusUpdateMessage extends BaseMessage{
 
     String recipientEmail;
