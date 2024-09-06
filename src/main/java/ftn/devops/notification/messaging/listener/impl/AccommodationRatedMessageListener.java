68494e15c00ddb1450145c63c4d06eae56453c60
Nadja Gvozdenac <gvozdenacn@gmail.com>
2024-09-06 18:50:29 +0200

    feat:Mail sender.

--- src/main/java/ftn/devops/notification/messaging/listener/impl/AccommodationRatedMessageListener.java
+++ src/main/java/ftn/devops/notification/messaging/listener/impl/AccommodationRatedMessageListener.java
@@ -1,22 +1,32 @@
 package ftn.devops.notification.messaging.listener.impl;
 
+import ftn.devops.notification.email.IEmailService;
 import ftn.devops.notification.messaging.listener.IMessageListener;
 import ftn.devops.notification.messaging.messages.AccommodationRatedMessage;
 import ftn.devops.notification.util.constants.AccommodationMessagingConstants.Queue;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class AccommodationRatedMessageListener implements IMessageListener<AccommodationRatedMessage> {
 
+    private static final String EMAIL_MESSAGE_FORMAT = "Your accommodation %s has been graded. \n Grade: %s.";
+
+    private static final String EMAIL_SUBJECT = "Your accommodation has been graded.";
+    
+    private final IEmailService emailService;
+    
     @Override
-    @RabbitListener(queues = Queue.RESERVATION_STATUS_UPDATE)
+    @RabbitListener(queues = Queue.ACCOMMODATION_RATED)
     public void receiveMessage(AccommodationRatedMessage message) {
         log.info("Received reservation status update message {}", message);
 
-        // TODO create email message object from received rabbitmq message
-        // TODO call email service send method
+        var emailText = String.format(EMAIL_MESSAGE_FORMAT, message.getAccommodationName(), message.getRating());
+
+        emailService.sendEmail(message.getRecipientEmail(), EMAIL_SUBJECT, emailText);
     }
 }
